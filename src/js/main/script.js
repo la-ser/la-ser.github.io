989b2546667b33e15da6fee4d1848ce3f557e16a
la-ser <77540680+la-ser@users.noreply.github.com>
2022-05-15 20:29:09 +0200

    Add files via upload

---
+++ src/js/main/script.js
@@ -0,0 +1,10 @@
+function colorMode() {
+    document.body.classList.toggle("colorMode")
+}
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    });
+}
