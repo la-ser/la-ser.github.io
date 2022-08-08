069f0b1dce212fbd5e21660f78dc7ebe8ede3cea
la-ser <77540680+la-ser@users.noreply.github.com>
2022-08-08 17:57:42 +0200

    Add files via upload

--- src/js/main/script.js
+++ src/js/main/script.js
@@ -11,8 +11,10 @@ function scrollToTop() {
 
 function openNav() {
     document.getElementById("sidenav-div").style.width = "250px";
+    document.getElementById("sidenav-back").classList.remove("hidden")
 }
 
 function closeNav() {
     document.getElementById("sidenav-div").style.width = "0";
+    document.getElementById("sidenav-back").classList.add("hidden")
 }
