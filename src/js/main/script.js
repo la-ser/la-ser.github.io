93f245c28d78b4fa585f9874fd53c0601fe086e6
la-ser <77540680+la-ser@users.noreply.github.com>
2022-08-08 18:12:34 +0200

    Add files via upload

--- src/js/main/script.js
+++ src/js/main/script.js
@@ -1,20 +1,24 @@
-function colorMode() {
-    document.body.classList.toggle("colorMode")
-}
 
-function scrollToTop() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth',
-    });
-}
+/*if (window.location !== window.parent.location) {
+    window.parent.location.replace("");
+}*/
+    function colorMode() {
+        document.body.classList.toggle("colorMode")
+    }
 
-function openNav() {
-    document.getElementById("sidenav-div").style.width = "250px";
-    document.getElementById("sidenav-back").classList.remove("hidden")
-}
+    function scrollToTop() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    }
 
-function closeNav() {
-    document.getElementById("sidenav-div").style.width = "0";
-    document.getElementById("sidenav-back").classList.add("hidden")
-}
+    function openNav() {
+        document.getElementById("sidenav-div").style.width = "250px";
+        document.getElementById("sidenav-back").classList.remove("hidden")
+    }
+
+    function closeNav() {
+        document.getElementById("sidenav-div").style.width = "0";
+        document.getElementById("sidenav-back").classList.add("hidden")
+    }
