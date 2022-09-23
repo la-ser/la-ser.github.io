41a39d021bb290584ffd1e24a3c5986b23c504ec
la-ser <77540680+la-ser@users.noreply.github.com>
2022-09-23 19:18:58 +0200

    Add files via upload

---
+++ src/js/main/script.js
@@ -0,0 +1,25 @@
+
+/*if (window.location !== window.parent.location) {
+    window.parent.location.replace("");
+}*/
+function colorMode() {
+    document.body.classList.toggle("colorMode");
+    document.body.scroll = "10"
+}
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    });
+}
+
+function openNav() {
+    document.getElementById("sidenav-div").style.width = "250px";
+    document.getElementById("sidenav-back").classList.remove("hidden")
+}
+
+function closeNav() {
+    document.getElementById("sidenav-div").style.width = "0";
+    document.getElementById("sidenav-back").classList.add("hidden")
+}
