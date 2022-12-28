9ca786c7ac3446c277e59f4f691623d67671aae3
la-ser <77540680+la-ser@users.noreply.github.com>
2022-12-28 21:25:14 +0100

    Add files via upload

--- src/js/script/links.js
+++ src/js/script/links.js
@@ -4,6 +4,7 @@ function loadlinks() {
         .then(data => {
             document.getElementById('link-discord').href = data.discord
             document.getElementById('link-arda').href = data.arda
+            document.getElementById('link-popup').href = data.popup
             document.getElementById('link-N/A').href = data.err
         })
 }
